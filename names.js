258ebd192fa4c35fe4a99a51286d6fe2c875772b
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2019-11-14 18:23:46 -0700

    added more girls names to girlNames array

--- names.js
+++ names.js
@@ -4,4 +4,7 @@ var boyNames = ['Liam', 'Noah', 'William', 'James', 'Logan', 'Benjamin', 'Mason'
 'Grayson', 'Jack', 'Julian', 'Levi', 'Christopher', 'Joshua', 'Andrew', 'Lincoln', 'Mateo', 'Ryan', 'Jaxon', 'Nathan', 
 'Aaron', 'Isaiah', 'Thomas', 'Charles', 'Caleb'];
 
-var girlNames = ['Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Charlotte', 'Mia', 'Amelia', 'Harper', 'Evelyn', 'Abigail']
+var girlNames = ['Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Charlotte', 'Mia', 'Amelia', 'Harper', 'Evelyn', 'Abigail', 
+'Emily', 'Elizabeth', 'Mila', 'Ella', 'Avery', 'Sofia', 'Camila', 'Aria', 'Scarlett', 'Victoria', 'Madison', 'Luna', 'Grace',
+'Chloe', 'Penelope', 'Layla', 'Riley', 'Zoey', 'Nora', 'Lily', 'Eleanor', 'Hannah', 'Lillian', 'Addison', 'Aubrey', 'Ellie', 
+'Stella', 'Natalie', 'Zoe', 'Leah', 'Hazel', 'Violet', 'Aurora', 'Savannah', 'Audrey', 'Brooklyn', 'Bella', 'Claire', 'Skylar'];
